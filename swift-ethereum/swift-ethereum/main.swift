ebd70db84e030d97bd0d85aa7694c9d1d93dc9fe
Valtteri Koskivuori <vkoskiv@gmail.com>
2018-03-19 03:55:13 +0200

    Add project template

---
+++ swift-ethereum/swift-ethereum/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-ethereum
+//
+//  Created by Valtteri on 19/03/2018.
+//  Copyright © 2018 vkoskiv. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
